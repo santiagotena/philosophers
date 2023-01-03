9b1f8bb7ebb2e4dd894255e6a83d7dc1c99451f0
Santiago Tena Hernandez <stenahe@gmail.com>
2023-01-03 23:23:05 +0100

    Relabel and add struct

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 20:11:34 by stena-he          #+#    #+#             */
-/*   Updated: 2023/01/03 18:57:03 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/01/03 23:14:24 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,27 @@
 # include <pthread.h>
 
 /* Structs */
-//Input data 
-typedef struct s_data
+//Parameters 
+typedef struct s_param
 {
 	int				number_of_philosophers;
 	int				time_to_die;
 	int				time_to_eat;
 	int				time_to_sleep;
 	int				number_of_times_each_philosopher_must_eat;
-}					t_data;
+}					t_param;
+
+typedef struct s_philo
+{
+	int				number_of_philosophers;
+}					t_philo;
 
 /* Functions */
 // Pipe setup //
 // int		pipes_setup(int argc, char **argv, char **envp);
 
-// Execute commands //
+// Utils //
+
 
 
 
