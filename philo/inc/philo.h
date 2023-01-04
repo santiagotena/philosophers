e45a712e10a13a17530c9fa6116a6a29e6d92f33
stena-he <stena-he@student.42wolfsburg.de>
2023-01-04 19:51:34 +0100

    Update structs and add function

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 20:11:34 by stena-he          #+#    #+#             */
-/*   Updated: 2023/01/04 01:55:37 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/01/04 19:45:29 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,29 @@
 //Parameters 
 typedef struct s_param
 {
-	int				number_of_philosophers;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				number_of_times_must_eat;
+	int					number_of_philosophers;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					number_of_times_must_eat;
+
+	int					i;
+	struct t_philo 		*philos;
+	pthread_t			*th;
+	pthread_mutex_t		*mutex;
 }					t_param;
 
 typedef struct s_philo
 {
-	int				number_of_philosophers;
-}					t_philo;
+	int					number_of_philosophers;
+	struct t_param 		*param;
+}						t_philo;
 
 /* Functions */
 // Parse Arguments //
 int		parse_args(int argc, char **argv, t_param *param);
+
+// Philosophers //
+int		philosophers(t_param *param);
 
 #endif
