0293e1d79eebd9ca811837652d5f14dbe6e27223
stena-he <stena-he@student.42wolfsburg.de>
2023-01-09 19:22:34 +0100

    Norm

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 20:11:34 by stena-he          #+#    #+#             */
-/*   Updated: 2023/01/09 01:09:23 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/01/09 19:21:39 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,7 @@
 typedef struct s_philo
 {
 	struct s_param		*param;
-	
 	int					philo_id;
-	int					t_to_die;
-	int					t_to_eat;
-	int					t_to_sleep;
 	int					ts_must_eat;
 }						t_philo;
 
@@ -41,7 +37,7 @@ typedef struct s_param
 	int					time_to_eat;
 	int					time_to_sleep;
 	int					times_must_eat;
-	time_t				start_time;
+	unsigned long long	start_time;
 
 	t_philo				*philos;
 	pthread_t			*th;
@@ -52,23 +48,23 @@ typedef struct s_param
 
 /* Functions */
 // Parse Arguments //
-int		parse_args(int argc, char **argv, t_param *param);
+int					parse_args(int argc, char **argv, t_param *param);
 
 // Philosophers //
-int		philosophers(t_param *param);
+int					philosophers(t_param *param);
 
 // Sole Philosopher //
-void	sole_philo(int time_to_die);
+void				sole_philo(int time_to_die);
 
 // Actions //
-void	take_fork(t_philo *philo);
-void	eat(t_philo *philo);
-void	sleeping(t_philo *philo);
-void	think(t_philo *philo);
-void	die(t_philo *philo);
+void				take_fork(t_philo *philo);
+void				eat(t_philo *philo);
+void				sleeping(t_philo *philo);
+void				think(t_philo *philo);
+void				die(t_philo *philo);
 
 // Utils //
-time_t	get_time_in_ms(void);
-void	philo_sleep(time_t sleep_time, t_param *param);
+unsigned long long	get_time_in_ms(void);
+void				philo_sleep(unsigned long long sleep_time, t_param *param);
 
 #endif
