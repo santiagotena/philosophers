4deb23e88b427296f334e6951700427cdada074a
stena-he <stena-he@student.42wolfsburg.de>
2023-01-10 00:05:38 +0100

    Rearrange and add notes

--- philo/src/actions.c
+++ philo/src/actions.c
@@ -6,11 +6,20 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 22:41:06 by stena-he          #+#    #+#             */
-/*   Updated: 2023/01/09 22:18:44 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/01/09 23:48:24 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	die(t_philo *philo)
+{
+	unsigned long long	time;
+	
+	philo->param->is_philo_dead = 1;
+	time = get_time_in_ms() - philo->param->start_time;
+	printf("%llu ms: %d died\n", time, philo->philo_id);
+}
 
 void	take_fork(t_philo *philo)
 {	
@@ -30,6 +39,7 @@ void	eat(t_philo *philo)
 		return ;
 	time = get_time_in_ms() - philo->param->start_time;
 	printf("%llu ms: %d is eating\n", time, philo->philo_id);
+	// Add time of meal
 	philo_sleep(philo->param->time_to_eat, philo->param);
 }
 
@@ -53,13 +63,5 @@ void	think(t_philo *philo)
 	time = get_time_in_ms() - philo->param->start_time;
 	printf("%llu ms: %d is thinking\n", time, philo->philo_id);
 	// time to think cacl + sleep
-}
-
-void	die(t_philo *philo)
-{
-	unsigned long long	time;
-	
-	philo->param->is_philo_dead = 1;
-	time = get_time_in_ms() - philo->param->start_time;
-	printf("%llu ms: %d died\n", time, philo->philo_id);
+	// perpetual thinking
 }
