56da2451cb2f016f0c3a78ff231de659c5b84dbc
Santiago Tena Hernandez <stenahe@gmail.com>
2023-01-09 03:17:56 +0100

    Modify printed messages

--- philo/src/actions.c
+++ philo/src/actions.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 22:41:06 by stena-he          #+#    #+#             */
-/*   Updated: 2023/01/09 01:14:00 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/01/09 03:16:43 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,43 @@
 
 void	take_fork(t_philo *philo)
 {	
-	printf("ms %d has taken a fork\n", philo->philo_id);
-
-	// t_param		*param;
-	// time_t		st;
+	time_t	time;
 	
-	// param = philo->param;
-	// st = param->start_time;
-	// printf("%ld", param->start_time);
+	time = 0; // current_time = get_time_in_ms() - start_time;
+	printf("%ld ms: %d has taken a fork\n", time, philo->philo_id);
 }
 
 void	eat(t_philo *philo)
 {
-	printf("ms %d is eating\n", philo->philo_id);
+	time_t	time;
+	
+	time = 0; // current_time = get_time_in_ms() - start_time;
+	printf("%ld ms: %d is eating\n", time, philo->philo_id);
+	// sleep
 }
 
 void	sleeping(t_philo *philo)
 {
-	printf("ms %d is sleeping\n", philo->philo_id);
+	time_t	time;
+	
+	time = 0; // current_time = get_time_in_ms() - start_time;
+	printf("%ld ms: %d is sleeping\n", time, philo->philo_id);
+	// sleep
 }
 
 void	think(t_philo *philo)
 {
-	printf("ms %d is thinking\n", philo->philo_id);
+	time_t	time;
+	
+	time = 0; // current_time = get_time_in_ms() - start_time;
+	printf("%ld ms: %d is thinking\n", time, philo->philo_id);
+	// time to think cacl + sleep
 }
 
 void	die(t_philo *philo)
 {
-	printf("ms %d died\n", philo->philo_id);
+	time_t	time;
+	
+	time = 0; // current_time = get_time_in_ms() - start_time;
+	printf("%ld ms: %d died\n", time, philo->philo_id);
 }
-
-// time_t		current_time;
-// usleep(time_to_die * 1000);
-// current_time = get_time_in_ms() - start_time;
-// printf("%ld ms: %d has died\n", current_time, philo->philo_id);
