9f3d132bb3813a51233ce046043c29c99a4ba4b7
stena-he <stena-he@student.42wolfsburg.de>
2023-01-10 22:02:20 +0100

    Norm

--- philo/src/fork_actions.c
+++ philo/src/fork_actions.c
@@ -6,13 +6,13 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 17:26:22 by stena-he          #+#    #+#             */
-/*   Updated: 2023/01/10 21:32:31 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/01/10 22:01:06 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-bool		grab_own_fork(t_philo *philo)
+bool	grab_own_fork(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->fork_lock);
 	if (philo->is_fork_taken == 0)
@@ -28,18 +28,16 @@ bool		grab_own_fork(t_philo *philo)
 	}
 }
 
-bool		grab_next_fork(t_philo *philo)
+bool	grab_next_fork(t_philo *philo)
 {
 	t_philo		*philo_next;
 	int			philo_id;
-	
+
 	philo_id = philo->philo_id;
-	
 	if (philo_id == philo->param->n_philo)
 		philo_next = &philo->param->philos[1];
 	else
 		philo_next = &philo->param->philos[philo_id + 1];
-
 	pthread_mutex_lock(&philo_next->fork_lock);
 	if (philo_next->is_fork_taken == 0)
 	{
@@ -63,12 +61,10 @@ void	drop_forks(t_philo *philo)
 	int			philo_id;
 
 	philo_id = philo->philo_id;
-	
 	if (philo_id == philo->param->n_philo)
 		philo_next = &philo->param->philos[1];
 	else
 		philo_next = &philo->param->philos[philo_id + 1];
-	
 	pthread_mutex_lock(&philo->fork_lock);
 	philo->is_fork_taken = 0;
 	pthread_mutex_unlock(&philo->fork_lock);
