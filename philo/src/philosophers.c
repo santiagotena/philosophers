2526c1100cd32490f3bace6f2a358360173ee232
stena-he <stena-he@student.42wolfsburg.de>
2023-01-13 03:52:59 +0100

    Setup forks array and assign next_philo_id

--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 17:58:15 by stena-he          #+#    #+#             */
-/*   Updated: 2023/01/13 01:15:55 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/01/13 03:44:17 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	join_threads(t_param *param)
 	int		i;
 
 	i = 0;
-	while (i < (param->n_philo + 1))
+	while (i <= param->n_philo)
 	{
 		if (pthread_join(param->th[i], NULL) != 0)
 		{
@@ -35,7 +35,7 @@ int	init_threads(t_param *param)
 
 	i = 1;
 	param->start_time = get_time_in_ms();
-	while (i < (param->n_philo + 1))
+	while (i <= param->n_philo)
 	{
 		if (pthread_create(&param->th[i], NULL, \
 			&routine, &param->philos[i]) != 0)
@@ -61,6 +61,7 @@ int	init_mutex(t_param *param)
 	while (i <= param->n_philo)
 	{
 		pthread_mutex_init(&param->philos[i].fork_lock, NULL);
+		pthread_mutex_init(&param->forks[i], NULL);
 		i++;
 	}
 	pthread_mutex_init(&param->msg_mutex, NULL);
@@ -77,12 +78,17 @@ void	init_values(t_param *param)
 	int		i;
 
 	param->th = malloc(sizeof(pthread_t) * (param->n_philo + 1));
-	param->philos = malloc(sizeof(t_philo) * param->n_philo);
+	param->philos = malloc(sizeof(t_philo) * (param->n_philo + 1));
+	param->forks = malloc(sizeof(pthread_mutex_t) * (param->n_philo + 1));
 	param->hungry_philo = param->n_philo;
 	i = 1;
 	while (i <= param->n_philo)
 	{
 		param->philos[i].philo_id = i;
+		if (i == param->n_philo)
+			param->philos[i].next_philo_id = 1;
+		else
+			param->philos[i].next_philo_id = i + 1;
 		param->philos[i].ts_must_eat = param->times_must_eat;
 		param->philos[i].is_fork_taken = 0;
 		param->philos[i].param = param;
