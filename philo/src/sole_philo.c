917630bfe9f0fb5420375296271b2741b8c94943
stena-he <stena-he@student.42wolfsburg.de>
2023-01-20 18:45:52 +0100

    Add color to output

--- philo/src/sole_philo.c
+++ philo/src/sole_philo.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 22:45:32 by stena-he          #+#    #+#             */
-/*   Updated: 2023/01/09 22:01:23 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/01/20 18:44:57 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@ void	sole_philo(int time_to_die)
 
 	start_time = get_time_in_ms();
 	current_time = get_time_in_ms() - start_time;
-	printf("%llu ms: 1 has taken a fork\n", current_time);
+	printf(ANSI_COLOR_YELLOW "%llu ms: 1 has taken a fork\n" \
+		ANSI_COLOR_RESET, current_time);
 	ft_sleep(time_to_die);
 	current_time = get_time_in_ms() - start_time;
-	printf("%llu ms: 1 has died\n", current_time);
+	printf(ANSI_COLOR_RED "%llu ms: 1 died\n" \
+		ANSI_COLOR_RESET, current_time);
 }
